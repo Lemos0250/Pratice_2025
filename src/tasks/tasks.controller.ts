3b4e1a5d272c310fb0c1d543c0a6a269dd323234
João Victor Lemos <79033128+Lemos0250@users.noreply.github.com>
2025-04-17 16:41:15 -0300

    BD

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -6,12 +14,34 @@ export class TasksController {
   constructor(private readonly taskService: TasksService) {}
 
   @Get()
-  getTasks() {
-    return this.taskService.glistAllTasks();
+  findAllTasks() {
+    return this.taskService.findAll();
   }
 
-  @Get('/1')
-  getTeste(){
-    return this.taskService.findOneTask();
+  @Get(':id')
+  findOneTask(@Param('id') id: string) {
+    return this.taskService.findOne(id);
+  }
+
+  @Post()
+  createTask(@Body() body: any) {
+    console.log(body);
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return this.taskService.create(body);
+  }
+
+  @Patch(':id')
+  updateTask(@Param('id') id: string, @Body() body: any) {
+    console.log('Id: ', id);
+    console.log('Body: ', body);
+
+    return 'Atualizando tarefa...';
+  }
+
+  @Delete(':id')
+  deleteTask(@Param('id') id: string) {
+    console.log('Id Enviado: ' + id);
+    return 'Deletar a tarefa com id' + id;
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,12 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './entities/task.entity';
 
 @Injectable()
 export class TasksService {
-  glistAllTasks() {
-    return [{ id: 1, task: 'Comprar pão' }];
+  private tasks: Task[] = [
+    {
+      id: 1,
+      name: 'Seguir o Passos para aprender DB',
+      description: 'Aprendendo muito sobre programação',
+      completed: false,
+    },
+  ]
+
+  findAll() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return this.tasks;
   }
 
-  findOneTask(){
-    return 'Trefa Matheus Teste...'
+  findOne(id: string) {
+    return this.tasks.find((task) => task.id === Number(id));
   }
+
+  create(body: any) {
+    const newId = this.tasks.length + 1;
+    const newTask = {
+      id: newId,
+      ...body,
+    };
+
+    console.log('Cadastrado com Sucesso')
+  
+    this.tasks.push(newTask);
+    
+    return newTask;
+  }
+  
 }
